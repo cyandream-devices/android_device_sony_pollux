cca9f1f0b65fc5bfbcbbda062c105d96db101c37
yanniks <kontakt@yanniks.de>
2014-04-07 20:56:47 +0200

    initial CyanDream commit
    Change-Id: Id2fcdcbd5ab9afd91ad920c2458a436e160d5298

--- cd.mk
+++ cd.mk
@@ -1,15 +1,15 @@
 $(call inherit-product, device/sony/pollux/full_pollux.mk)
 
 # Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cyandream/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cyandream/config/nfc_enhanced.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321_1273-0364/SGP321:4.2.2/10.3.1.A.0.244/C_93rg:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 4.2.2 10.3.1.A.0.244 C_93rg test-keys"
 
-PRODUCT_NAME := cm_pollux
+PRODUCT_NAME := cd_pollux
 PRODUCT_DEVICE := pollux
